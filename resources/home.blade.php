a122b0db0a439a3f3371431b5150940d12ec031c
aouman <k2.aouman@gmail.com>
2020-10-27 12:29:44 +0000

    creation de la page d'accueil, mise en ligne des texte modèle, section nos prix. la suppression de certaines sections unitile

--- resources/home.blade.php
+++ resources/home.blade.php
@@ -3,8 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="assets/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 
@@ -38,7 +37,7 @@
       <div class="overlay"></div>
       <div class="container-fluid px-0">
       	<div class="row d-md-flex no-gutters slider-text align-items-center js-fullheight justify-content-end" style="height: 625px;">
-	      	<img class="one-third js-fullheight align-self-end order-md-last img-fluid" src="/imgs/pair.svg" alt="" style="height: 625px;">
+	      	<img class="one-third js-fullheight align-self-end order-md-last img-fluid" src="assets/imgs/pair.svg" alt="" style="height: 625px;">
 	        <div class="one-forth d-flex align-items-center ftco-animate js-fullheight fadeInUp ftco-animated" style="height: 625px;">
 	        	<div class="text mt-5">
 	        		<!--span class="subheading">Isometric Hosting</span-->
